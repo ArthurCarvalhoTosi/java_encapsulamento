21e7506bd75bc41daa45909bbb0180515d4e73b3
Arthur Tosi <arthurtosi07@hotmail.com>
2023-12-18 16:57:03 -0300

    first commit

---
+++ src/encapsula/controle/Controlador.java
@@ -0,0 +1,25 @@
+package encapsula.controle;
+
+public interface Controlador {
+
+	void abrirMenu();
+
+	void fecharMenu();
+
+	void maisVolume();
+
+	void menosVolume();
+
+	void ligarMudo();
+
+	void desligarMudo();
+
+	void play();
+
+	void pause();
+
+	void desligar();
+
+	void ligar();
+
+}
---
+++ src/encapsula/controle/Encapsulamento.java
@@ -0,0 +1,18 @@
+package encapsula.controle;
+
+public class Encapsulamento {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Controle_remoto c = new Controle_remoto();
+		c.ligar();
+		c.maisVolume();
+		c.abrirMenu();
+		c.menosVolume();
+		c.menosVolume();
+		c.menosVolume();
+		c.maisVolume();
+		c.abrirMenu();
+	}
+
+}
